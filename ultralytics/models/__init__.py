7ed3ca59b01a3c326cda4ff25c99fbe1d259632f
benderick <shuoguo6666abc@163.com>
2025-04-11 00:29:57 +0800

    添加模块

--- ultralytics/models/__init__.py
+++ ultralytics/models/__init__.py
@@ -1,9 +1,10 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-from .fastsam import FastSAM
-from .nas import NAS
-from .rtdetr import RTDETR
-from .sam import SAM
-from .yolo import YOLO, YOLOWorld
+# from .fastsam import FastSAM
+# from .nas import NAS
+# from .rtdetr import RTDETR
+# from .sam import SAM
+# from .yolo import YOLOWorld
+from .yolo import YOLO
 
-__all__ = "YOLO", "RTDETR", "SAM", "FastSAM", "NAS", "YOLOWorld"  # allow simpler import
+__all__ = "YOLO"  # allow simpler import
